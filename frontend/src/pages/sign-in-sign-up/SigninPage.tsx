d40e08aa409d21b42dc345663d35dc1b1286c075
Khaled Rakhisi <32028547+khaledrakhisi@users.noreply.github.com>
2022-02-23 23:07:03 +0330

    Update SigninPage.tsx

--- frontend/src/pages/sign-in-sign-up/SigninPage.tsx
+++ frontend/src/pages/sign-in-sign-up/SigninPage.tsx
@@ -9,7 +9,6 @@ const SigninSignup: React.FunctionComponent = () => {
   return (
     <React.Fragment>
       <div className="signin-signup-page">
-        
         <Route exact path="/login" component={Signin}/>        
       </div>
     </React.Fragment>
